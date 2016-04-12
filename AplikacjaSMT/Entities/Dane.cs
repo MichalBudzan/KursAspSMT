af57fdea924bbe45ed72a3ddfd275d16a3ac2ee7
MichalBudzan <200294@student.pwr.edu.pl>
2016-04-12 17:34:47 +0200

    init

---
+++ AplikacjaSMT/Entities/Dane.cs
@@ -0,0 +1,15 @@
+﻿namespace AplikacjaSMT.Entities
+{
+    [Table("Dane")]
+    public class Dane
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [StringLength(1024)]
+        public string Name { get; set; }
+
+        public byte[] ImageBytes { get; set; }
+
+    }
+}
